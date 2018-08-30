c092a753ac9372bb56b0fdb7f8adee195276b72f
lipouli <li.lipou@gmail.com>
2018-08-30 09:27:03 +0200

    hovered style for realisations

--- src/components/Pages/Realisations/Project/Section.js
+++ src/components/Pages/Realisations/Project/Section.js
@@ -1,9 +1,16 @@
 import styled from 'react-emotion';
 
+import { hover } from '~/styles/color';
+
 const Section = styled.section`
   border: 1px solid red;
   padding: 0.3rem;
   margin-bottom: 0.3rem;
+  transition: 0.3s;
+  cursor: pointer;
+  &:hover {
+    border: 1px solid ${hover};
+  }
 `;
 
 export default Section;
