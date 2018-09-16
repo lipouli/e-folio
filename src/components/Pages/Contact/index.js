091ee88e703020d3213a91d67672b66e7154682e
lipouli <li.lipou@gmail.com>
2018-09-16 08:32:49 +0200

    change linkedin url

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -9,7 +9,7 @@ const Contact = () => (
   <Div className="section">
     <h2>Me contacter :</h2>
     <p><FontAwesomeIcon icon={faAt} /> <a href="mailto:li.lipou@gmail.com">li.lipou@gmail.com</a></p>
-    <p><FontAwesomeIcon icon={faLinkedin} /> <a target="_blanck" rel="noopener noreferrer" href="https://www.linkedin.com/in/lipou-li-861a1a145/">Ma page Linkedin</a></p>
+    <p><FontAwesomeIcon icon={faLinkedin} /> <a target="_blanck" rel="noopener noreferrer" href="https://www.linkedin.com/in/lipouli">Ma page Linkedin</a></p>
     <p><FontAwesomeIcon icon={faGithub} /> <a target="_blanck" rel="noopener noreferrer" href="https://github.com/lipouli">Ma page Github</a></p>
   </Div>
 );
