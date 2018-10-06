a0e0754832876b91d4c1c36fa3a1237ace36cb66
lipouli <li.lipou@gmail.com>
2018-10-06 22:20:02 +0200

    correction

--- src/projects/Click/Description.js
+++ src/projects/Click/Description.js
@@ -5,7 +5,7 @@ import Div from '~/projects/Styles/description';
 const Description = () => (
   <Div>
     <p>Ceci est une application de création et d'envoi de facture automatique</p>
-    <p>Pour le moment non terminé mais sera optomisé peu à peu par moi même</p>
+    <p>Pour le moment non terminé mais sera optimisé peu à peu par moi même</p>
     <p>Symfony en Back et React en Front</p>
     <p>Base de donnée permettant d'enregistrer ses produits, clients, factures</p>
     <p>Equipe de 4 personnes</p>
